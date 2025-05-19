fcb730b270d4f6c319683c9d2e51f87f77f771c0
rduartesilva <rduartesilva@ipvc.pt>
2025-05-19 16:43:00 +0100

    Set up scene builder and java FX for design

---
+++ Salto do Cavalo/src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("initialScene.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.setMainWindow(primaryStage);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
