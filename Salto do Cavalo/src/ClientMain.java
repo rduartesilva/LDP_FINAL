cf229450a91ce788d7ff6c2744e32447f2497c93
rduartesilva <rduartesilva@ipvc.pt>
2025-06-18 17:11:54 +0100

    Jogo finalizado

--- Salto do Cavalo/src/ClientMain.java
+++ Salto do Cavalo/src/ClientMain.java
@@ -13,7 +13,15 @@ public class ClientMain extends Application {
 
         Controller controller = loader.getController();
 
+        Player player1 = new Player("Duarte", 4, 0, '1', true);
+        Player player2 = new Player("André", 0, 4, '2', false);
+        Horse horse1 = new Horse(4, 0);
+        Horse horse2 = new Horse(0, 4);
+
         controller.startGameClient();
+        controller.setPlayers(player1, player2);
+        controller.setHorses(horse1, horse2);
+
 
         primaryStage.setTitle("Salto do Cavalo - Jogador");
         primaryStage.setScene(new Scene(root));
--- Salto do Cavalo/src/Main.java
+++ Salto do Cavalo/src/Main.java
@@ -8,34 +8,13 @@ public class Main extends Application {
     @Override
    public void start(Stage primaryStage) {
     try {
-        FXMLLoader serverLoader = new FXMLLoader(getClass().getResource("ServerScene.fxml"));
-        Parent rootServer = serverLoader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ServerScene.fxml"));
+        Parent root = loader.load();
         Stage serverStage = new Stage();
         serverStage.setTitle("Servidor - Salto do Cavalo");
-        serverStage.setScene(new Scene(rootServer));
+        serverStage.setScene(new Scene(root));
         serverStage.show();
         System.out.println("Servidor iniciado");
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("initialScene.fxml"));
-        System.out.println("Carregando initialScene.fxml...");
-        Parent root = loader.load();
-        System.out.println("initialScene.fxml carregado com sucesso!");
-
-        Controller controller = loader.getController();
-
-        Player player1 = new Player("Duarte", 4, 0, '1', true);
-        Player player2 = new Player("André", 0, 4, '2', false);
-
-        Horse horse1 = new Horse(4, 0);
-        Horse horse2 = new Horse(0, 4);
-
-        controller.setPlayers(player1, player2);
-        controller.setHorses(horse1, horse2);
-        controller.setMainWindow(primaryStage);
-
-        primaryStage.setTitle("Salto do Cavalo");
-        primaryStage.setScene(new Scene(root, 800, 800));
-        primaryStage.show();
         System.out.println("Janela do tabuleiro aberta");
 
     } catch (Exception e) {
